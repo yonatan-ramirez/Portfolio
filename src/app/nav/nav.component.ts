eb62e3f1bf208afdd73584d59ffabf035a7e0103
Yonatan <yonatan.ramirez557@gmail.com>
2018-07-11 16:52:58 -0700

    update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 @Output() navSelected = new EventEmitter<string>();
-
+  logo: string;
   navSelect(feature: string) {
     this.navSelected.emit(feature);
   }
-  constructor() { }
+  constructor() {
+    this.logo = '../../assets/images/yrmlogotilt.png';
+  }
 
   ngOnInit() {
   }
